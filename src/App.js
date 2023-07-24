50d0577fedd66f61ead1299723276a77bc163a74
ishaan1028 <ishaandhawane77@gmail.com>
2023-07-24 18:00:49 +0530

    travis setup

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello yall! welcome to react</p>
-        <p>Hello yall! welcome to react</p>
-        <p>Hello yall! welcome to react</p>
+        <p>Hello yall! travis build</p>
         <a
           className="App-link"
           href="https://reactjs.org"
